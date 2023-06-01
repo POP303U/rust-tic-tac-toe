b0c9af5319952d107b859f97c40b554dbd2dba9d
POP303U <nicoanton.gering@gmail.com>
2023-06-01 21:55:17 +0200

    removing target folder

--- target/debug/deps/cfg_if-91585cb6680b7958.d
+++
@@ -1,7 +0,0 @@
-/home/android/programming/rust/cargo/rust-tic-tac-toe/target/debug/deps/cfg_if-91585cb6680b7958.rmeta: /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/android/programming/rust/cargo/rust-tic-tac-toe/target/debug/deps/libcfg_if-91585cb6680b7958.rlib: /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/android/programming/rust/cargo/rust-tic-tac-toe/target/debug/deps/cfg_if-91585cb6680b7958.d: /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
--- target/debug/rust-tic-tac-toe.d
+++
@@ -1,1 +0,0 @@
-/home/android/programming/rust/cargo/rust-tic-tac-toe/target/debug/rust-tic-tac-toe: /home/android/programming/rust/cargo/rust-tic-tac-toe/src/main.rs
