ce09bf17987d476ca8cc87e14b711dca38592ce2
POP303U <nicoanton.gering@gmail.com>
2023-06-01 21:49:59 +0200

    added .gitignore for target

--- target/debug/deps/ppv_lite86-5c786d73432f564b.d
+++
@@ -1,8 +0,0 @@
-/home/android/programming/rust/cargo/rust-tic-tac-toe/target/debug/deps/ppv_lite86-5c786d73432f564b.rmeta: /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/generic.rs
-
-/home/android/programming/rust/cargo/rust-tic-tac-toe/target/debug/deps/ppv_lite86-5c786d73432f564b.d: /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/generic.rs
-
-/home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs:
-/home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs:
-/home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs:
-/home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/generic.rs:
--- target/debug/deps/rand_chacha-e24f5c8a8b09e428.d
+++
@@ -1,7 +0,0 @@
-/home/android/programming/rust/cargo/rust-tic-tac-toe/target/debug/deps/rand_chacha-e24f5c8a8b09e428.rmeta: /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/android/programming/rust/cargo/rust-tic-tac-toe/target/debug/deps/rand_chacha-e24f5c8a8b09e428.d: /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
-/home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
-/home/android/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
